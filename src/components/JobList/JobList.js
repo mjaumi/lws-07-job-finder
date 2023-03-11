0d10e60c7d070e36439a0710a8091ba24b2f310e
mjaumi <mjaumi2864@gmail.com>
2023-03-11 19:33:13 +0600

    fixed joblist header

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -83,7 +83,7 @@ const JobList = () => {
     return (
         <>
             <div className='md:flex space-y-2 md:space-y-0 justify-between mb-10 '>
-                <h1 className='lws-section-title'>{filterBy === 'All' ? 'All Available Jobs' : `${filterBy} Jobs`}</h1>
+                <h1 className='lws-section-title'>{filterBy === 'All' ? 'All Available Jobs' : `Available ${filterBy} Jobs`}</h1>
                 <div className='flex gap-4'>
                     <div className='search-field group flex-1'>
                         <i className='fa-solid fa-magnifying-glass search-icon group-focus-within:text-blue-500'></i>
